cd9c90fce2d3101171656583150283241a30f9e2
“Dorce <dorce.raphael@gmail.com>
2021-06-27 08:23:18 +0200

    update files

---
+++ index.js
@@ -0,0 +1,54 @@
+const app = require("express")();
+const PORT = 3000;
+const request = require("request");
+const cheerio = require("cheerio");
+const cron = require("node-cron");
+// var fs = require("fs");
+
+var jsonDiv = document.querySelector(".json-div");
+
+// TEST CRON
+// cron.schedule("*/5 * * * * *", () => {
+//   console.log("test");
+// });
+
+// TEST REST API
+app.listen(PORT, () => console.log(`it's alive on http://localhost:${PORT}`));
+
+app.get("/test", (req, res) =>
+  request(
+    "https://www.sportmag.fr/sport-handi",
+    function (error, response, body) {
+      if (error) {
+        res.send(response.statusCode);
+      }
+      var article = [];
+      var $ = cheerio.load(body);
+      $(".edgtf-peis-image-holder").each(function (index, element) {
+        article[index] = {};
+        article[index]["img"] = $(element)
+          .find(".edgtf-peis-link")
+          .attr("href");
+      });
+
+      $(".edgtf-peis-content-holder").each(function (index, element) {
+        article[index]["titre"] = $(element)
+          .find(".edgtf-peis-title")
+          .text()
+          .trim();
+        article[index]["auteur"] = $(element)
+          .find(".edgtf-post-info-author-link")
+          .attr("href");
+        article[index]["date"] = $(element)
+          .find(".edgtf-post-info-date a span")
+          .text();
+        article[index]["descr"] = $(element).find(".edgtf-post-excerpt").text();
+      });
+
+      res.json(article);
+      jsonDiv.innerHTML = res.json(article);
+      // var joe = res.json(article);
+      // fs.writeFile("myjsonfile.json", joe, "utf8", callback);
+    }
+  )
+);
